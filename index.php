e01e88f8cb4d2c359f2691b90b1e6970cb840879
samuelbourdon <samuel.bourdon@gmail.com>
2015-03-12 13:57:25 +0100

    Add basic theme files

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package harbour
+ */
+
+get_header(); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<?php //get_template_part( 'content', 'none' ); ?>
+			<p>Nothing here</p>
+
+		<?php endif; ?>
+
+		</main>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
